d04181f729921bfe90db5009e5f3a1a505c9dcca
SalvoBevi <salvo.bevi@gmail.com>
2020-06-26 15:24:46 +0000

    h2 style edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ alert ("Hi, I'm Javascripst");
 
 var namePlayer = prompt ("Please enter your name:")
 
-function namePlayer() {
-  document.getElementById("subtitle").innerHTML ="Welcome " + namePlayer + "! How are you today?";
-};
+function myFunction() {
+  document.getElementById("subtitle").innerHTML = ("Welcome " + namePlayer);
+}
