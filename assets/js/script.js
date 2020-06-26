61120109a4adfa2583b85fd9e87819004418079d
SalvoBevi <salvo.bevi@gmail.com>
2020-06-26 14:44:38 +0000

    Script edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,4 @@ var namePlayer = prompt ("Please enter your name:")
 
 function namePlayer() {
   document.getElementById("subtitle").innerHTML ="Welcome " + namePlayer + "! How are you today?";
-}
+};
