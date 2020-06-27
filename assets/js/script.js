bffb5636a2e846c3c749b01fba7a8fc0284ed730
SalvoBevi <salvo.bevi@gmail.com>
2020-06-27 17:20:49 +0000

    prompt command edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 alert ("Hi, I'm the Memory Game");
+
+var namePlayer = prompt("Please enter your name", "<name goes here>");
+
+function welcomeName () {
+     $(namePlayer!= null)
+    document.getElementById("welcome").innerHTML =
+    "Welcome " + namePlayer;
+}
