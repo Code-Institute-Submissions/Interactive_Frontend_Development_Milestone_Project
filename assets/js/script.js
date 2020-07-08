5e4fa37402649f56fc80a32cd8b599fc3f6fb49e
SalvoBevi <salvo.bevi@gmail.com>
2020-07-08 14:34:36 +0000

    index update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-alert ("Hi, I'm the Memory Game");
-
-//var namePlayer = prompt("Please enter your name:", "Name...");
-
-const cards = document.querySelectorAll('.memory-card');
-
 function flipCard() {
   this.classList.toggle('flip');
 }
