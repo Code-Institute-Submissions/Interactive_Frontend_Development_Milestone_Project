186b99bd0b1071bdbddf57f2fa1ed010daad4b61
SalvoBevi <salvo.bevi@gmail.com>
2020-07-08 14:20:17 +0000

    flip funciont added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,10 @@ alert ("Hi, I'm the Memory Game");
 
 //var namePlayer = prompt("Please enter your name:", "Name...");
 
+const cards = document.querySelectorAll('.memory-card');
 
+function flipCard() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
