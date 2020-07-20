b044ac495ecbeea4a736d062675e36114478f5c9
SalvoBevi <salvo.bevi@gmail.com>
2020-07-20 16:09:11 +0000

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,46 +4,38 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+//-----------------------------------------------FlipCard Function
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-
   this.classList.add('flip');
-
   if (!hasFlippedCard) {
-    // first click
     hasFlippedCard = true;
     firstCard = this;
-
     return;
   }
-
-  // second click
   secondCard = this;
-
   checkForMatch();
 }
 
+//-----------------------------------------------Match Function
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
   isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
   resetBoard();
 }
 
 function unflipCards() {
   lockBoard = true;
-
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
   }, 750);
 }
@@ -52,6 +44,8 @@ function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
+
+//-----------------------------------------------shuffle Function
 
 (function shuffle() {
   cards.forEach(card => {
